51b64ff7c7dcf893ce08395a0128c59344b1c6a8
Eugene Hardbread <agnivalent@gmail.com>
2018-04-20 16:08:15 +0200

    changed credits for myself

--- src/cljs/dcd_website/credits.cljs
+++ src/cljs/dcd_website/credits.cljs
@@ -1,20 +1,20 @@
 (ns dcd-website.credits)
 
 (def dcd-team
-  [["Carlo Sciolla" "https://twitter.com/skuro"]
-   ["Vijay Kiran" "https://twitter.com/vijaykiran"]
-   ["Eugene Lukyanchuk" "https://github.com/agnivalent"]
-   ["Joost Diepenmaat" "https://twitter.com/ZeekatSoftware"]])
+  [["Carlo Sciolla" "https://twitter.com/skuro" "twitter"]
+   ["Vijay Kiran" "https://twitter.com/vijaykiran" "twitter"]
+   ["Gene Lukyanchuk" "mailto:agnivalent@gmail.com" "email"]
+   ["Joost Diepenmaat" "https://twitter.com/ZeekatSoftware" "twitter"]])
 
 (defn credits []
   [:span
    [:div
-    (letfn [(organizer [[name link]]
+    (letfn [(organizer [[name link link-type]]
               ^{:key link}
               [:span.org-info
                [:a {:href link
                     :target :_blank}
-                [:img.icon.twitter {:src "img/icons/twitter.svg"}]
+                [:img.icon.twitter {:src (str "img/icons/" link-type ".svg")}]
                 name]])]
       (map organizer dcd-team))]
    [:h3 "Design & Art"]
